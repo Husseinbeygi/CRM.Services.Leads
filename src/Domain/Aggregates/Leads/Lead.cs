6d3ad26174cc0ddab0c1641e8577526c325f8075
Hussein Beygi <hussein1997.hb@hotmail.com>
2022-11-12 21:32:52 +0330

    Added Lead UnitTest

--- src/Domain/Aggregates/Leads/Lead.cs
+++ src/Domain/Aggregates/Leads/Lead.cs
@@ -1,38 +1,79 @@
 ﻿using Domain.Aggregates.Leads.ValueObjects;
+using Domain.SharedKernel;
 
 namespace Domain.Aggregates.Leads;
 
 public class Lead : SeedWork.AggregateRoot
 {
+	public Lead(Guid tenantId, Salutation salutation,
+		FirstName firstName, LastName lastName, EmailAddress email, LeadStatus status,
+		string title, string company, string mobile,
+		string phone, Rating rating, string country, string state,
+		string city, string street, Industry industry, string annualRevenue,
+		LeadSource leadSource, string postalCode, int? numberOfEmployees, string website, string description)
+	{
 
-        public SharedKernel.FirstName FirstName { get; set; }
-        public SharedKernel.LastName LastName { get; set; }
-        public SharedKernel.FullName FullName { get; set; }
-        public string Company { get; set; }
-        public SharedKernel.EmailAddress Email { get; set; }
-        public string Title { get; set; }
-        public string Mobile { get; set; }
-        public string Phone { get; set; }
-        public Guid OwnerId { get; set; }
-        public long ModifiedAt { get; set; }
-        public Guid ModifiedById { get; set; }
-        public long CreatedAt { get; set; }
-        public Guid CreatedById { get; set; }
-        public LeadStatus Status { get; set; }
-        public string AnnualRevenue { get; set; }
-        public string City { get; set; }
-        public string Country { get; set; }
-        public string Description { get; set; }
-        public Industry Industry { get; set; }
-        public LeadSource LeadSource { get; set; }
-        public int NumberOfEmployees { get; set; }
-        public string PostalCode { get; set; }
-        public Rating Rating { get; set; }
-        public SharedKernel.Salutation Salutation { get; set; }
-        public string State { get; set; }
-        public string Street { get; set; }
-        public string Website { get; set; }
-        public Guid TenantId { get; set; }
 
+		LastName = lastName ?? throw new ArgumentNullOrEmptyException
+			(string.Format(Resources.Messages.Validations.Required, Resources.DataDictionary.LastName));
+
+		Company = company ?? throw new ArgumentNullOrEmptyException
+			(string.Format(Resources.Messages.Validations.Required, Resources.DataDictionary.CompanyName));
+
+		#region AssignTheFields
+		FirstName = firstName;
+		Email = email;
+		Title = title;
+		Mobile = mobile;
+		Phone = phone;
+		LeadStatus = status;
+		AnnualRevenue = annualRevenue;
+		City = city;
+		Country = country;
+		Description = description;
+		Industry = industry;
+		LeadSource = leadSource;
+		NumberOfEmployees = numberOfEmployees;
+		PostalCode = postalCode;
+		Rating = rating;
+		State = state;
+		Street = street;
+		Website = website;
+		TenantId = tenantId;
+		Salutation = salutation;
+		CreatedAt = Framework.DateTime.DateTime.GetCurrentUnixUTCTimeMilliseconds();
+		#endregion
+	}
+
+	#region Property(ies)
+	public SharedKernel.FirstName FirstName { get; private set; }
+	public SharedKernel.LastName LastName { get; private set; }
+	public string Company { get; private set; }
+	public SharedKernel.EmailAddress Email { get; private set; }
+	public string Title { get; private set; }
+	public string Mobile { get; private set; }
+	public string Phone { get; private set; }
+	public Guid OwnerId { get; private set; }
+	public long ModifiedAt { get; private set; }
+	public Guid ModifiedById { get; private set; }
+	public long CreatedAt { get; private set; }
+	public Guid CreatedById { get; private set; }
+	public LeadStatus LeadStatus { get; private set; }
+	public string AnnualRevenue { get; private set; }
+	public string City { get; private set; }
+	public string Country { get; private set; }
+	public string Description { get; private set; }
+	public Industry Industry { get; private set; }
+	public LeadSource LeadSource { get; private set; }
+	public int? NumberOfEmployees { get; private set; }
+	public string PostalCode { get; private set; }
+	public Rating Rating { get; private set; }
+	public SharedKernel.Salutation Salutation { get; private set; }
+	public string State { get; private set; }
+	public string Street { get; private set; }
+	public string Website { get; private set; }
+	public Guid TenantId { get; private set; }
+
+	#endregion /Property(ies)
 
 }
---
+++ test/Domain.Test/Aggregates/Leads/LeadUnitTest.cs
@@ -0,0 +1,87 @@
+﻿using Domain.Aggregates.Leads.ValueObjects;
+using Domain.SharedKernel;
+
+namespace Domain.Test.Aggregates.Leads;
+
+public class LeadUnitTest
+{
+	[Theory]
+	[InlineData(null, null)]
+	[InlineData("Beygi",null)]
+	[InlineData(null, "TestComp")]
+	public void ShouldThrowExceptionWhenRequiredInputIsNotValid(string Lname,string Company)
+	{
+		var result = () =>
+			new Domain.Aggregates.Leads.Lead
+			(Guid.NewGuid(),
+			null,
+			null, LastName.Create(Lname), null, null, null,
+			company: Company, mobile: null, phone: null, rating: null, country: null, state: null, city: null, street: null, industry: null, annualRevenue: null, leadSource: null, postalCode: null
+, numberOfEmployees: null, website: null, description: null);
+
+
+		result.Should().Throw<Exception>();
+	}
+
+	[Theory]
+	[InlineData("Beygi", "TestComp")]
+	[InlineData("    Beygi       ", "        TestComp          ")]
+	[InlineData("    Beygi   ", "        Test    Comp          ")]
+	public void ShouldCreateUserWhenMinimumRequiredInputAreValid(string Lname, string Company)
+	{
+		var result = 
+			new Domain.Aggregates.Leads.Lead
+			(Guid.NewGuid(),
+			null,
+			null, LastName.Create(Lname), null, null, null,
+			company: Company, mobile: null, phone: null, rating: null, country: null, state: null, city: null, street: null, industry: null, annualRevenue: null, leadSource: null, postalCode: null
+, numberOfEmployees: null, website: null, description: null);
+
+
+		result.LastName.Should().Be(LastName.Create("Beygi"));
+		result.Company.Should().Be(Company);
+	}
+
+	[Fact]
+	public void ShouldCreateUserProperly()
+	{
+		var result =
+			new Domain.Aggregates.Leads.Lead
+			(Guid.NewGuid(),
+			Salutation.Mr,
+			FirstName.Create("Hussein"),
+			LastName.Create("beygi"),
+			EmailAddress.Create("Hussein@Gmail.com"), LeadStatus.New, "CEO",
+			company: "Company", mobile: "+989152020056", phone: "123456789",
+			rating: Rating.Warm, country: "Iran", state: "Khorasan",
+			city: "Mashhad", street: "Saba", industry: Industry.Technology, annualRevenue: "120000",
+			leadSource: LeadSource.Web, postalCode: "123123123", numberOfEmployees: 0, website: "Goolds.com",
+			description: "Very Good");
+
+
+		result.FirstName.Value.Should().Be("Hussein");
+		result.LastName.Value.Should().Be("beygi");
+		result.Salutation.Value.Should().Be(0);
+		result.LeadSource.Value.Should().Be(5);
+		result.Rating.Value.Should().Be(1);
+		result.LeadStatus.Value.Should().Be(0);
+		result.Company.Should().Be("Company");
+		result.Title.Should().Be("CEO");
+		result.Mobile.Should().Be("+989152020056");
+		result.Phone.Should().Be("123456789");
+		result.AnnualRevenue.Should().Be("120000");
+		result.City.Should().Be("Mashhad");
+		result.Country.Should().Be("Iran");
+		result.Description.Should().Be("Very Good");
+		result.Industry.Value.Should().Be(24);
+		result.State.Should().Be("Khorasan");
+		result.NumberOfEmployees.Should().Be(0);
+		result.PostalCode.Should().Be("123123123");
+		result.Street.Should().Be("Saba");
+		result.Website.Should().Be("Goolds.com");
+		result.Email.Value.Should().Be("Hussein@Gmail.com");
+
+	}
+
+
+}
