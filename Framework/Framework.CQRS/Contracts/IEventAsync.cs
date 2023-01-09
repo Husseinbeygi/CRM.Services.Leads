46fbb0344dc3dbefc92b623b5cd927c83cef8a3e
Hussein Beygi <hussein1997.hb@hotmail.com>
2023-01-09 20:14:34 +0330

    Added CancellationToekn

--- Framework/Framework.CQRS/Contracts/IEventAsync.cs
+++ Framework/Framework.CQRS/Contracts/IEventAsync.cs
@@ -8,7 +8,7 @@
 	public interface IEventHandlerAsync<TEvent>
 								where TEvent : IEventAsync
 	{
-		Task HandleAsync(TEvent @event);
+		Task HandleAsync(TEvent @event,CancellationToken cancellationToken = default);
 	}
 
 }
